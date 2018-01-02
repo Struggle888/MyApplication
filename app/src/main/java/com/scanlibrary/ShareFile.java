bc1373ea188e087f0fbdc3a4b863c217bef7c255
Administrator <136447572@qq.com>
2018-01-02 11:34:44 +0800

    four-commit

---
+++ app/src/main/java/com/scanlibrary/ShareFile.java
@@ -0,0 +1,27 @@
+package com.scanlibrary;
+
+public class ShareFile {
+
+	public static final String USERFILE="userInfoFile";
+	public static final String ISLOGIN="isLogin";
+	public static final String ISOKSERVICE="isokservice";
+	public static final String UID="userid";
+	public static final String PHONE="phone";
+	public static final String USERNAME="username";
+	public static final String POINT="point";
+	public static final String HEADIMAGE="HeadImg"; //头像
+	public static final String ISCHECK ="IsCheck"; //0 待认证  1 已驳回   2 已认证
+	public static final String ISENGINEER ="isEngineer"; //0 不是服务商  1 是服务商
+
+	public static final String LOGINTIME="loginTime";
+
+	public static final String ENGAREA="EngArea";
+
+	public static final String DONGJIE="Dongjie";  //冻结 1 未冻结 0 已冻结
+
+	public static final String LOCATIONPERSTION ="locationperstion";
+
+	public static final String SWITCH ="switch";  //0 关 1 开
+
+	public static final String ISINITIAL ="isInitial" ;  //0 关 1 开
+}
